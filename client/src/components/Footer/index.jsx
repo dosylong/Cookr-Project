ac4d1503210f13cf196596d2b22896e5541a639c
dosylong <dosylong3011@gmail.com>
2021-10-10 16:00:36 +0700

    update footer and main layout

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
+import { Link } from '@mui/material';
+import Box from '@mui/material/Box';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+          {'Copyright © '}
+            <Link color="inherit" href="/">
+              Cookr
+            </Link>{' '}
+              {new Date().getFullYear()}{'.'}
+        </Typography>
+  );
+}
 
 export default function Footer() {
   return (
-    <Typography variant="h1">
-      
-    </Typography>
+    <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Cookr
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Some text about footer
+        </Typography>
+        <Copyright />
+    </Box>
   )
 }
--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -3,13 +3,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-export default function MainPage() {
+export default function MainPage({children}) {
   return (
     <>
       <CssBaseline />
-        <Header />
-    
-        <Footer />
+      <Header />
+        {children}
+      <Footer />
     </>
   );
 }
